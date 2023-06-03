77bdc747140ad72d02db3040449362e5b7aced34
Doris <130676335+KT-Designer@users.noreply.github.com>
2023-06-04 04:19:01 +0800

    Add files via upload

---
+++ js/slides_per_view.js
@@ -0,0 +1,41 @@
+var swiper = new Swiper(".mySwiper", {
+    // 自動播放
+    autoplay: {
+        delay: 1000,
+        disableOnInteraction: false,
+    },
+    // 第一張與最後一張無縫連接
+    loop: true,
+    // 顯示數量
+    slidesPerView: 5,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+    },
+
+    // 斷點
+    breakpoints: {
+        // when window width is >= 375px
+        375: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 40
+        }
+    }
+
+});
